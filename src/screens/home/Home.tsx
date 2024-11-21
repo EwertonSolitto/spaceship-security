f7314ac352461bf3c53ca93f0fb061612ed51b56
Ewerton <ewerton2004@yahoo.com.br>
2024-11-21 15:47:09 -0300

    feat: organize first files

---
+++ src/screens/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import styles from './HomeStyles';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Spaceship Security</Text>
+    </View>
+  );
+}
