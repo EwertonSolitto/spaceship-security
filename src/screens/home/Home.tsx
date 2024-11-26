2f3253bd6391633900d161e873323cbf335f6509
Ewerton <ewerton2004@yahoo.com.br>
2024-11-25 21:33:40 -0300

    feat: create config menu

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,21 +1,50 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {Alert, Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import * as Clipboard from "expo-clipboard";
+import Checkbox from 'expo-checkbox';
 
 import styles from './HomeStyles';
 import { StatusBar } from 'expo-status-bar';
 import passwordGenerator from '../../scripts/passwordGenerator';
+import TCharacters from '../../types/TCharacters';
 
 export default function Home() {
   const [password, setPassword] = useState('')
+  const [openedMenu, setOpenedMenu] = useState(false)
+  const [isNumberChecked, setIsNumberChecked] = useState(true)
+  const [isCapitalLetterChecked, setIsCapitalLetterChecked] = useState(true)
+  const [isLowercaseLetterChecked, setIsLowercaseLetter] = useState(true)
+  const [isSpecialChecked, setIsSpecialChecked] = useState(true)
+  const [passwordLength, setPasswordLength] = useState('8')
+
+  useEffect(() => {
+    if(passwordLength <= '0') {
+      setPasswordLength('1')
+    }
+    
+    if(isNumberChecked === false && isCapitalLetterChecked === false && isCapitalLetterChecked === false && isSpecialChecked === false) {
+      Alert.alert("", "Leave at least one checkbox checked")
+    }
+  })
 
   function createPassword() {
-    const newPassword = passwordGenerator(['numbers', 'capital letters', 'lowercase letters', 'special'], 8)
+    setOpenedMenu(false)
+    const optionsSelected: TCharacters[] = []
 
-    setPassword(newPassword)
+    if(isNumberChecked) optionsSelected.push('numbers')
+    if(isCapitalLetterChecked) optionsSelected.push('capital letters')
+    if(isLowercaseLetterChecked) optionsSelected.push('lowercase letters')
+    if(isSpecialChecked) optionsSelected.push('special')
+
+    if(optionsSelected.length) {
+      const newPassword = passwordGenerator(optionsSelected, Number(passwordLength))
+      setPassword(newPassword)
+    }
   }
 
   function copyPassword() {
+    setOpenedMenu(false)
+    
     Clipboard.setStringAsync(password)
     Alert.alert("", "Password copied successfully!")
   }
@@ -36,10 +65,66 @@ export default function Home() {
       <TouchableOpacity style={styles.button} onPress={createPassword}>
         <Text style={styles.buttonText}>🚀 Generate Password</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={copyPassword}>
+      <TouchableOpacity style={styles.button} onPress={copyPassword} disabled={password ? false : true}>
         <Text style={styles.buttonText}>Copy</Text>
       </TouchableOpacity>
 
+      <TouchableOpacity onPress={() => setOpenedMenu(true)}>
+        <Text style={styles.configButton}>Config</Text>
+      </TouchableOpacity>
+
+      <View style={[styles.menu, openedMenu ? styles.opened : styles.closed]}>
+        <Text style={styles.configTitle}>Config</Text>
+        <View style={styles.checkboxContainer}>
+          <Checkbox
+            style={styles.checkbox}
+            value={isNumberChecked}
+            onValueChange={setIsNumberChecked}
+            color={isNumberChecked ? '#03050B' : undefined}
+          />
+          <Text style={styles.checkboxText}>Numbers</Text>
+        </View>
+        <View style={styles.checkboxContainer}>
+          <Checkbox
+            style={styles.checkbox}
+            value={isCapitalLetterChecked}
+            onValueChange={setIsCapitalLetterChecked}
+            color={isCapitalLetterChecked ? '#03050B' : undefined}
+          />
+          <Text style={styles.checkboxText}>Capital Letters</Text>
+        </View>
+        <View style={styles.checkboxContainer}>
+          <Checkbox
+            style={styles.checkbox}
+            value={isLowercaseLetterChecked}
+            onValueChange={setIsLowercaseLetter}
+            color={isLowercaseLetterChecked ? '#03050B' : undefined}
+          />
+          <Text style={styles.checkboxText}>Lowercase Letters</Text>
+        </View>
+        <View style={styles.checkboxContainer}>
+          <Checkbox
+            style={styles.checkbox}
+            value={isSpecialChecked}
+            onValueChange={setIsSpecialChecked}
+            color={isSpecialChecked ? '#03050B' : undefined}
+          />
+          <Text style={styles.checkboxText}>Special (!@#$%...)</Text>
+        </View>
+        <View style={styles.inputConfigMenu}>
+          <Text style={styles.inputConfigTitle}>Number of Characters:</Text>
+          <TextInput
+            style={styles.inputConfig}
+            keyboardType='number-pad'
+            onChangeText={text => setPasswordLength(text)}
+            value={passwordLength}
+            maxLength={3}
+          />
+        </View>
+        <TouchableOpacity style={styles.closeButton} onPress={() => setOpenedMenu(false)}>
+          <Text style={styles.closeText}>Close</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/screens/home/HomeStyles.tsx
+++ src/screens/home/HomeStyles.tsx
@@ -16,7 +16,8 @@ const styles = StyleSheet.create({
     width: 248,
     height: 248,
     borderRadius: 248,
-    marginBlock: 16
+    marginTop: 16,
+    marginBottom: 32
   },
   text: {
     color: colors.white,
@@ -25,8 +26,10 @@ const styles = StyleSheet.create({
   input: {
     backgroundColor: colors.purple,
     width: 248,
-    marginTop: 8,
-    borderRadius: 10
+    marginTop: 16,
+    borderRadius: 10,
+    height: 44,
+    padding: 8
   },
   button: {
     backgroundColor: colors.yellow,
@@ -41,6 +44,82 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 20,
     fontWeight: 700,
+    textAlign: "center"
+  },
+  configButton: {
+    fontSize: 16,
+    color: colors.white,
+    marginTop: 24,
+    textDecorationLine: "underline"
+  },
+  menu: {
+    display: "flex",
+    position: "absolute",
+    width: 312,
+    backgroundColor: colors.white,
+    borderWidth: 8,
+    borderColor: colors.yellow,
+    borderRadius: 24,
+    justifyContent: "center",
+    alignItems: "flex-start",
+    gap: 10,
+    paddingInline: 16,
+    paddingBottom: 24,
+    paddingTop: 16
+  },
+  configTitle: {
+    fontSize: 28,
+    fontWeight: 700,
+    marginInline: "auto"
+  },
+  checkboxContainer: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  checkbox: {
+    borderColor: colors.yellow,
+  },
+  checkboxText: {
+    fontSize: 16,
+    marginLeft: 4,
+    fontWeight: 500
+  },
+  inputConfigMenu: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  inputConfigTitle: {
+    fontSize: 16,
+    fontWeight: 500,
+    marginRight: 4
+  },
+  inputConfig: {
+    backgroundColor: colors.black,
+    color: colors.white,
+    fontSize: 16,
+    borderRadius: 4,
+    width: 34,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  opened: {
+    display: "flex"
+  },
+  closed: {
+    display: "none"
+  },
+  closeButton: {
+    marginInline: 'auto',
+    backgroundColor: colors.black,
+    paddingBlock: 8,
+    width: 124,
+    marginTop: 16,
+    borderRadius: 12
+  },
+  closeText: {
+    color: colors.white,
+    fontSize: 16,
     textAlign: "center"
   }
 });
